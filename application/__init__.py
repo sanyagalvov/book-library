a27896ae68e58e3addde5c860b51c3286c729bb4
sanyagalvov <37501131+sanyagalvov@users.noreply.github.com>
2020-05-02 10:57:43 +0300

    Revert "wrong commit"
    This reverts commit c8f312fe2e92f0aab6eb762528a85b828ceb1466.

--- application/__init__.py
+++ application/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,7 +10,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-bootstrap = Bootstrap(app)
 
 from application import routes, models
 
